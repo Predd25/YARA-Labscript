857652473d83be89d68b9d83e4d8e0ccb4e1cc91
Patrick R <42159304+Predd25@users.noreply.github.com>
2024-05-08 23:58:49 -0600

    Create lab.sh

---
+++ lab.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Hello there! I'm a{virus}" > ~/Desktop/scaryfile.txt
+echo "You shouldn't have opened me... Now you're in {trouble} Boo{3652}" > ~/Cursed_Cipher.txt
+echo "The long road home seemed to go on and on. Boo{3652}" > /tmp/tempest.txt
+
+
+mkdir ~/Yaralab/yarGen/malwarez 
+echo "VGhpcyBpcyBub3QgdGhlIGRyb2lkIHlvdSBhcmUgbG9va2luZyBmb3Iu 3652. EF. AB. #%%%^Bob##Jones" > ~/Yaralab/yarGen/malwarez/Sinister_Script.txt
